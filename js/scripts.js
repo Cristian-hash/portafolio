f87d233128199996a86abb1dc1fb0b0428caacc6
Crhistian Ziegler Pacori Paucar <65138605+Cristian-hash@users.noreply.github.com>
2023-02-28 21:36:36 -0500

    Cambios nuevos de color

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+const toggleTheme = document.getElementById('toggl-theme');
+const toggleIcon = document.getElementById('toggl-icon');
+const toggleText = document.getElementById('toggl-text');
+
+toggleTheme.addEventListener('click',()=>{
+
+    
+})
