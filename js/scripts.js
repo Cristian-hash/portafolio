36413f98f54e28403af77db4fee71fde1510ebc6
Crhistian Ziegler Pacori Paucar <cpacori@unsa.edu.pe>
2023-03-06 17:08:47 -0500

    Correcion de errores min 21.20

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,22 @@
-const toggleTheme = document.getElementById('toggle-theme');
-const toggleIcon = document.getElementById('toggle-icon');
-const toggleText = document.getElementById('toggle-text');
+const toggleTheme = document.getElementById("toggle-theme");
+const toggleIcon = document.getElementById("toggle-icon");
+const toggleText = document.getElementById("toggle-text");
 
 const toogleColors = document.getElementById("toggle-colors");
 
-toggleTheme.addEventListener('click',()=>{
-    document.body.classList.toggle('dark');
-    if(toggleIcon.src.includes('moon.svg')){
-        toggleIcon.src='assets/icons/sun.svg'
-        toggleText.textContent='Light Mode'
-    }else{
-        toggleIcon.src='assets/icons/moon.svg'
-        toggleText.textContent='Dark Mode'
+const rootStyles = document.documentElement.style;
+
+toggleTheme.addEventListener("click",() => {
+    document.body.classList.toggle("dark");
+    if(toggleIcon.src.includes("moon.svg")) {
+        toggleIcon.src="assets/icons/sun.svg";
+        toggleText.textContent="Light Mode";
+    } else {
+        toggleIcon.src = "assets/icons/moon.svg";
+        toggleText.textContent="Dark Mode";
     }
 });
 
 toggleColors.addEventListener('click',(e)=>{
-    console.log(e.target.dataset);
-})
+    rootStyles.setProperty('--primary-color',e.target.dataset.color);
+});
