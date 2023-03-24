887b55f459f647e7ffff5c9813e890acc9f63b4f
Crhistian Ziegler Pacori Paucar <65138605+Cristian-hash@users.noreply.github.com>
2023-03-23 23:05:02 -0500

    Cambios laptop 15:49

--- js/scripts.js
+++ js/scripts.js
@@ -4,19 +4,6 @@ const toggleText = document.getElementById("toggle-text");
 
 const toogleColors = document.getElementById("toggle-colors");
 
-<<<<<<< HEAD
-const rootStyles = document.getElement.style;
-
-
-toggleTheme.addEventListener('click',()=>{
-    document.body.classList.toggle('dark');
-    if(toggleIcon.src.includes('moon.svg')){
-        toggleIcon.src='assets/icons/sun.svg'
-        toggleText.textContent='Light Mode'
-    }else{
-        toggleIcon.src='assets/icons/moon.svg'
-        toggleText.textContent='Dark Mode'
-=======
 const rootStyles = document.documentElement.style;
 
 toggleTheme.addEventListener("click", () => {
@@ -27,15 +14,9 @@ toggleTheme.addEventListener("click", () => {
     } else {
         toggleIcon.src = "assets/icons/moon.svg";
         toggleText.textContent="Dark Mode";
->>>>>>> 65810e33fd3f492deb5eb35cb7391aa201ad3177
     }
 });
 
 toggleColors.addEventListener('click',(e)=>{
-<<<<<<< HEAD
-    rootStyles.setProperty ('--primary-color',e.target.dataset.color);
-})
-=======
     rootStyles.setProperty('--primary-color', e.target.dataset.color);
 });
->>>>>>> 65810e33fd3f492deb5eb35cb7391aa201ad3177
