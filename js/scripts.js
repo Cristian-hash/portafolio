1e46924eb2fc15f5079f995ec3d989a0103bf6b1
Crhistian Ziegler Pacori Paucar <65138605+Cristian-hash@users.noreply.github.com>
2024-01-20 15:30:36 -0500

    corregido identacion

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,24 @@
 const toggleTheme = document.getElementById("toggle-theme");
 const toggleIcon = document.getElementById("toggle-icon");
 const toggleText = document.getElementById("toggle-text");
-
 const toggleColors = document.getElementById("toggle-colors");
-
 const rootStyles = document.documentElement.style;
 
+// Verificar si los elementos existen antes de acceder a sus propiedades
 toggleTheme.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
-    if(toggleIcon.src.includes("moon.svg")) {
-        toggleIcon.src="assets/icons/sun.svg";
-        toggleText.textContent="Light Mode";
-    } else {
-        toggleIcon.src = "assets/icons/moon.svg";
-        toggleText.textContent="Dark Mode";
-    }
+  document.body.classList.toggle("dark");
+  if (toggleIcon.src.includes("moon.svg")) {
+    // Cambiar el icono y el texto según el tema actual
+    toggleIcon.src = "assets/icons/sun.svg";
+    toggleText.textContent = "Light Mode";
+  } else {
+    toggleIcon.src = "assets/icons/moon.svg";
+    toggleText.textContent = "Dark Mode";
+  }
 });
 
-toggleColors.addEventListener('click',(e)=>{
-    rootStyles.setProperty('--primary-color', e.target.dataset.color);
+// Agregar un evento de clic para cambiar el color principal según el elemento clicado
+toggleColors.addEventListener("click", (e) => {
+  // Validar si el elemento clicado tiene el atributo 'data-color'
+  rootStyles.setProperty("--primary-color", e.target.dataset.color);
 });
